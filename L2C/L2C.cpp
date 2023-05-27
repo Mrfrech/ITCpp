ad7b939ed67b68b98515dbe0e4fee259db8733aa
Mrfrech <dakshbharalgen@gmail.com>
2023-05-27 17:05:12 +0530

    abouts comments

---
+++ L2C/L2C.cpp
@@ -0,0 +1,10 @@
+//Comments in cpp file 
+
+//Comments are the text that is ignored by the program or is the the part of the program.
+//Uses of the comments are simple, we use comment for our understanding purposes, like writing some important information or explaining some part of the program.
+
+//We can use comment by adding prefix '//' to text
+
+/*
+  this is a multi line comment, 
+*/
