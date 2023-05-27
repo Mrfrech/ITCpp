82bbccd38d431f98ce4c246b87f6a9b295b65549
Mrfrech <dakshbharalgen@gmail.com>
2023-05-27 14:38:39 +0530

    complete first lecture code

--- 1VSC/1VSC.cpp
+++ 1VSC/1VSC.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
+#include <iostream> // <iostream is the pre-built libaray used for print our program in the console
 
-int main() {
-    std::cout << "hello world" << std::endl;
+int main() { //This main() function is the entry point of our program
+             //An entry point is a program where the execution of a program begins
+    std::cout << "hello world" << std::endl; //std::cout is a part of <iostream> library and its shows output to the console
+    std::cout << "number1" << std::endl;
+    std::cout << "number2";
+    std::cout << "number3" << std::endl;     //std::endl is for adding new line after printing "hello world" 
     return 0;
 }
